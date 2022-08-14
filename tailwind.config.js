2697defbd0cb124ac141e7c3d0869e92f815cf57
Korchev <Korchev94@gmail.com>
2022-08-14 19:19:09 +0100

    FIX: font-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,18 @@ module.exports = {
   //prettier-ignore
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './pages/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './components/*.{js,ts,jsx,tsx}'],
+    ],
   theme: {
-    fontFamily: {
-      oswald: ['Oswald', 'sans-serif'],
-    },
     letterSpacing: {
       wider: '0.25em',
       widest: '.15em',
       giant: '0.3em',
     },
     extend: {
+      fontFamily: {
+        oswald: ['Oswald', 'sans-serif'],
+      },
       colors: { darkgray: '#21222d', customgray: '#2f3136', customyellow: '#ffb600', customdarkgray: '#181a1e' },
     },
   },
